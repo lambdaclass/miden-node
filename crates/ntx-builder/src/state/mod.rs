bd74768659d181ff967658301b156ccf8274605c
Bernhard Schuster <bernhard@ahoi.io>
2025-10-23 18:24:42 +0200

    fix: trim MAX_NOTE_ATTEMPTS to 1 (#1307)
    Reduces noise significantly on testnet, and we don't have many practical
    error cases _yet_. In the future we should increment it again.

--- crates/ntx-builder/src/state/mod.rs
+++ crates/ntx-builder/src/state/mod.rs
@@ -90,7 +90,7 @@ pub struct State {
 
 impl State {
     /// Maximum number of attempts to execute a network note.
-    const MAX_NOTE_ATTEMPTS: usize = 30;
+    const MAX_NOTE_ATTEMPTS: usize = 1;
 
     /// Load's all available network notes from the store, along with the required account states.
     #[instrument(target = COMPONENT, name = "ntx.state.load", skip_all)]
